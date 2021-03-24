fdc6a44d1aace86167a8952d5135062dd61ea505
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-03-24 17:36:34 -0300

    #5: Criar o componente TotalOfInteras

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intera/presentation/home/components/total_of_interas.dart';
 import '../../shared/extensions/screen_util_extension.dart';
 import 'components/app_bar/app_bar.dart';
 
@@ -7,6 +8,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeAppBar(),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
+          child: Column(
+            children: [
+              TotalOfInteras(total: 10),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
